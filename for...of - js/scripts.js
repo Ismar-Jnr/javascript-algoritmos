d253cde93ab4135b1e32d102df0fabbd0f6c57db
José Ismar <ismar.jr@icloud.com>
2024-03-28 22:15:03 -0300

    for...of - js
    entendendo sobre for...of em js

---
+++ for...of - js/scripts.js
@@ -0,0 +1,12 @@
+//for...of
+
+let name = 'Ismar';
+let names = ['João', 'Paulo', 'Pedro'];
+
+for(let char of name) {
+    console.log(char);
+}
+
+for(let name of names){
+    console.log(name);
+}
