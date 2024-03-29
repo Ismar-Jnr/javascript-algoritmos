907b947b84f2b165c04c28ff3fd0c7c28c484237
José Ismar <ismar.jr@icloud.com>
2024-03-28 21:58:05 -0300

    for - js
    estrutura de repetição for em js

---
+++ for (repetition structure) js/scripts.js
@@ -0,0 +1,15 @@
+// Estrutura de repetição
+// for
+
+
+
+// break; => para a execução do loop
+// continue; => pula a execução do momento
+for(let i = 10; i > 0; i--){
+    
+
+    if(i === 5){
+        continue; // "continue;" pula o resto dos comandos do loop e retorna no começo, dando seguimento normal ao resto do loop, caso ainda tenho repetições a fazer;
+    }
+    console.log(i)
+}
